e94f0213b05b37a57cb8862be32cf0398cfae95f
norhanmagdi <58911005+norhanmagdi@users.noreply.github.com>
2021-06-05 15:32:45 +0200

    left recursion & factoring

--- include/readGrammar.h
+++ include/readGrammar.h
@@ -15,9 +15,12 @@ class readGrammar
         void setNonTerminals(set<string> t);
         vector<pair<string,vector<string>>> get_splited_grammar_set();
         vector<pair<string,vector<string>>> leftRecursion(const vector<pair<string,vector<string>>> terms);
+        vector<pair<string,vector<string>>> leftFactoring(const vector<pair<string,vector<string>>> terms);
         bool containLeftRecursion(pair<string,vector<string>> term);
         vector<pair<string,vector<string>>> solveLeftRecursion(pair<string,vector<string>> term);
-        void print();
+        pair<string,vector<string>>indirectLeftRec(pair<string,vector<string>> termi,pair<string,vector<string>> termj);
+        vector<pair<string,vector<string>>>solveLeftFactoring(pair<string,vector<string>> term);
+        void writeFile(vector<pair<string,vector<string>>> finalGrammar);
 
 
 
